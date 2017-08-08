f508839b0d2b3f5732397591e672bea0ece3bec6
Mikhail Sergeev <mrhuman007@gmail.com>
2017-08-08 18:44:45 +0500

    not tested

---
+++ task1.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Михаил
+ * Date: 08.08.2017
+ * Time: 15:18
+ */
+
+class RepeatingWordsFinder
+{
+    #Выводит строку, из которой состоят все слова аргумента или false,
+    #Если общей такой подстроки у слов не существует.
+    #Каждая строка разбивается на равные подстроки. Если эти подстроки одинаковы,
+    #То в $repeatingWords['подстрока'] прибавляется единица.
+    #Для 'abcabc' такие подстроки — 'abc', 'abcabc'.
+    #Если в каждом слове была найдена одинаковая подстрока, функция
+    #Вернет ее. Иначе функция вернет false. O(n^2) :(((((((
+    public function findRepeatingWords($wordsArray)
+    {
+        array_splice($wordsArray, 0, 1);
+        $repeatingWords = array();
+        foreach ($wordsArray as $word) {
+            $divs = array(1, strlen($word));
+            for ($i = 2; $i < strlen($word) / 2 + 1; $i++)
+                if (strlen($word) % $i == 0)
+                    $divs[] = $i;
+
+            $k = 0;
+            while ($k < count($divs)) {
+                $i = strlen($word) / $divs[$k++];
+                $chunks = str_split($word, $i);
+                if (count(array_unique($chunks)) == 1) {
+                    $repeatingWords[$chunks[0]] += 1;
+                }
+            }
+        }
+        if (in_array(count($wordsArray), $repeatingWords))
+            return array_search(count($wordsArray), $repeatingWords);
+        else
+            return false;
+    }
+}
+
+$finder = new RepeatingWordsFinder();
+echo $finder->findRepeatingWords($argv), "\n";
---
+++ task2.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Михаил
+ * Date: 08.08.2017
+ * Time: 15:18
+ */
+
+class MostVisitorsTimeFinder
+{
+    #Функция парсит строку в объекты DateTime и сравнивает их.
+    #Если время ухода i-того человека больше времени прихода k-того человека
+    #И меньше или равно времени ухода k-того человека, то их время пересекается
+    #В max(in)-min(out).
+    #Функция возвращает наибольшее число посетителей музея в один интервал времени
+    #И этот интервал в формате 'G:i'-'G:i' O(n^2) :((((((.
+    public function findMostVisitorsTime($pairsArray)
+    {
+        array_splice($pairsArray, 0, 1);
+        $ins = array();
+        $outs = array();
+
+        #Парсинг строки-аргумента в массивы DateTime
+        foreach ($pairsArray as $pair)
+        {
+            $inAndOut = explode('-', $pair);
+            $in = DateTime::createFromFormat('G:i', $inAndOut[0]);
+            $out = DateTime::createFromFormat('G:i', $inAndOut[1]);
+            $ins[] = $in;
+            $outs[] = $out;
+        }
+
+        $maxVisitors = 0;
+        $maxTimeIn = new DateTime();
+        $minTimeOut = new DateTime();
+
+        #
+        for ($i = 0; $i < count($outs); $i++)
+        {
+            $visitors = 0;
+            $inTime = new DateTime();
+            for ($j = 0; $j < count($ins); $j++)
+            {
+                if ($ins[$j] < $outs[$i] && $outs[$j] >= $outs[$i])
+                {
+                    $visitors++;
+                    $inTime = $ins[$j];
+                }
+            }
+            if ($maxVisitors < $visitors)
+            {
+                $maxVisitors = $visitors;
+                $maxTimeIn = $inTime;
+                $minTimeOut = $outs[$i];
+            }
+        }
+        return $maxVisitors.' '.$maxTimeIn->format('G:i').'-'.$minTimeOut->format('G:i');
+    }
+}
+
+$finder = new MostVisitorsTimeFinder();
+echo $finder->findMostVisitorsTime($argv), "\n";
